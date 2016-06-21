05999ff55c444bffd4e9565cab814bece3787522
Augusto Pedraza <augusto.pedraza08@gmail.com>
2016-06-21 15:45:34 -0300

    added minial solution and specs

--- lib/prime_factors_kata.rb
+++ lib/prime_factors_kata.rb
@@ -1,3 +1,21 @@
-module PrimeFactorsKata
+class PrimeFactorsKata
   # Your code goes here...
+  def descompose(number)
+    return [number] if is_prime? number
+
+    first_multiplier = calculate_minimal_multiplier number
+
+    ([first_multiplier] << descompose(number/first_multiplier)).flatten
+  end
+
+  private
+
+  def is_prime? n
+    calculate_minimal_multiplier(n).nil?
+  end
+
+  def calculate_minimal_multiplier(n)
+    (2...n).find { |i| n%i == 0 }
+  end
 end
+
--- spec/prime_factors_kata_spec.rb
+++ spec/prime_factors_kata_spec.rb
@@ -1,4 +1,54 @@
 require 'spec_helper'
 
 describe PrimeFactorsKata do
+  subject { described_class.new.descompose(number) }
+
+  context "when the input is 2" do
+
+    let(:number) { 2 }
+
+    it "returns the same input" do
+      expect(subject).to match([2])
+    end
+  end
+
+  context "when the input is 3" do
+    let(:number) { 3 }
+
+    it "returns the same input" do
+      expect(subject).to match([3])
+    end
+  end
+
+  context "when the input is 4" do
+    let(:number) { 4 }
+
+    it "returns its descomposition" do
+      expect(subject).to match([2, 2])
+    end
+  end
+
+  context "when the input is 6" do
+    let(:number) { 6 }
+
+    it "returns its descomposition" do
+      expect(subject).to match([2, 3])
+    end
+  end
+
+  context "when the input is 10" do
+    let(:number) { 10 }
+
+    it "returns its descomposition" do
+      expect(subject).to match([2, 5])
+    end
+  end
+
+  context "when the input is 27" do
+    let(:number) { 27 }
+
+    it "returns its descomposition" do
+      expect(subject).to match([3, 3, 3])
+    end
+  end
 end
